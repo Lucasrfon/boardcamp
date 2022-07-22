e462c92719a294a0a8c67d7ca4a0a26e41f1bbe6
Lucas Fonseca <lucasrfon@gmail.com>
2022-07-22 17:50:33 -0300

    implementa categoriesMiddleware

---
+++ src/middlewares/categoriesMiddleware.js
@@ -0,0 +1,27 @@
+import joi from "joi";
+import { connection } from "../dbStrategy/database.js";
+
+export function validateCategory(req, res, next) {
+    const catgorySchema = joi.object({
+        name: joi.string().required()
+    });
+    const category = req.body;
+    const validation = catgorySchema.validate(category);
+
+    if(validation.error) {
+        return res.status(400).send(validation.error.details[0].message)
+    }
+
+    next();
+}
+
+export async function validateUniqueName(req, res, next) {
+    const { name } = req.body;
+    const { rows: notUniqueName } = await connection.query(`SELECT * FROM categories WHERE name = '${name}'`);
+
+    if(notUniqueName.length === 1) {
+        return res.status(409).send('Categoria já cadastrada.')
+    }
+
+    next();
+}
--- src/routers/categoriesRouter.js
+++ src/routers/categoriesRouter.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { getCategories, postCategory } from "../controllers/categoriesController.js";
+import { validateCategory, validateUniqueName } from "../middlewares/categoriesMiddleware.js";
 
 const router = Router();
 
 router.get('/categories', getCategories);
-router.post('/categories', postCategory);
+router.post('/categories', validateCategory, validateUniqueName, postCategory);
 
 export default router;
