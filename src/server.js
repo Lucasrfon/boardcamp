b368b94092bd3bc971e2cf587b2936dbdc6a0950
Lucas Fonseca <lucasrfon@gmail.com>
2022-07-24 00:02:05 -0300

    implementa customersRouter

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import categoriesRouter from './routers/categoriesRouter.js';
 import gamesRouter from './routers/gamesRouter.js';
+import customersRouter from './routers/customersRouter.js';
 
 dotenv.config();
 
@@ -12,5 +13,6 @@ server.use(cors());
 
 server.use(categoriesRouter);
 server.use(gamesRouter);
+server.use(customersRouter);
 
 server.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}.`));
