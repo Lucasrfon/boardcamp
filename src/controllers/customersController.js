f771a2f1fadba6a796f7d59a74eba43f957900a2
Lucas Fonseca <lucasrfon@gmail.com>
2022-07-24 00:25:57 -0300

    implementa postCustomers

--- src/controllers/customersController.js
+++ src/controllers/customersController.js
@@ -12,3 +12,14 @@ export async function getCustomers(req, res) {
         res.status(500).send(error);
     }
 }
+
+export async function postCustomers(req, res) {
+    try {
+        const {name, phone, cpf, birthday} = req.body;
+        await connection.query(`
+        INSERT INTO customers (name, phone, cpf, birthday) VALUES ('${name}', '${phone}', '${cpf}', '${birthday}')`);
+        res.status(201).send();
+    } catch (error) {
+        res.status(500).send(error);
+    }
+}
--- src/routers/customersRouter.js
+++ src/routers/customersRouter.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { getCustomers } from "../controllers/customersController.js";
+import { getCustomers, postCustomers } from "../controllers/customersController.js";
 
 const router = Router();
 
 router.get('/customers', getCustomers);
-router.post('/customers');
+router.post('/customers', postCustomers);
 
 export default router;
